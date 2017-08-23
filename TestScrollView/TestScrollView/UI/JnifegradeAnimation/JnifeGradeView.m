3fbd595ad66957d46d73f7b04925cf8b5cf8463d
LiuQun <LMM@huijuquanqiu.local>
2017-08-23 16:02:56 +0800

    积分动画

--- TestScrollView/TestScrollView/UI/JnifegradeAnimation/JnifeGradeView.m
+++ TestScrollView/TestScrollView/UI/JnifegradeAnimation/JnifeGradeView.m
@@ -8,6 +8,9 @@
 
 #import "JnifeGradeView.h"
 #import "UIButton+BezierAnimation.h"
+#import "NSString+StringSize.h"
+
+
 
 @interface JnifeGradeView ()
 
@@ -36,12 +39,17 @@
     
     [self addSubview:self.gradeButton];
     
-    [self.gradeButton  mas_makeConstraints:^(MASConstraintMaker *make) {
-        
-        make.center.equalTo(self);
-        make.size.mas_equalTo(CGSizeMake(30, 30));
-        
-    }];
+    
+    [self.gradeButton setFrame:(CGRectMake(0, 0, 30, 30))];
+    
+    [self.gradeButton setCenter:CGPointMake(65, 15)];
+    
+//    [self.gradeButton  mas_makeConstraints:^(MASConstraintMaker *make) {
+//        
+//        make.center.equalTo(self);
+//        make.size.mas_equalTo(CGSizeMake(30, 30));
+//        
+//    }];
     
     
 }
@@ -56,6 +64,7 @@
         [_gradeButton setImage:[UIImage imageNamed:@"vip"] forState:(UIControlStateNormal)];
         [_gradeButton setBackgroundColor:[UIColor darkGrayColor]];
         [_gradeButton addTarget:self action:@selector(gradeButtonAction) forControlEvents:(UIControlEventTouchUpInside)];
+        [_gradeButton.titleLabel setFont:[UIFont systemFontOfSize:18]];
         [_gradeButton.layer setCornerRadius:15];
         [_gradeButton.layer setMasksToBounds:YES];
         
@@ -68,21 +77,21 @@
 
 - (void)gradeButtonAction{
     
-    [UIView animateWithDuration:0.3 delay:0 usingSpringWithDamping:0.35 initialSpringVelocity:0.9 options:
-     UIViewAnimationOptionCurveLinear animations:^{
-         
-         CGRect tempRect = self.gradeButton.frame;
-         
-         tempRect = CGRectMake(self.gradeButton.frame.origin.x, self.gradeButton.frame.origin.y, self.gradeButton.frame.size.width + 9, 30);
-         
-         [self.gradeButton setFrame:tempRect];
-         [self.gradeButton setCenter:self.center];
-         
-     } completion:^(BOOL finished) {
-         
-         
-         
-     }];
+//    [UIView animateWithDuration:0.3 delay:0 usingSpringWithDamping:0.35 initialSpringVelocity:0.9 options:
+//     UIViewAnimationOptionCurveLinear animations:^{
+//         
+//         CGRect tempRect = self.gradeButton.frame;
+//         
+//         tempRect = CGRectMake(self.gradeButton.frame.origin.x, self.gradeButton.frame.origin.y, self.gradeButton.frame.size.width + 9, 30);
+//         
+//         [self.gradeButton setFrame:tempRect];
+//         [self.gradeButton setCenter:self.center];
+//         
+//     } completion:^(BOOL finished) {
+//         
+//         
+//         
+//     }];
     
 }
 
@@ -95,7 +104,29 @@
     
 //    [self.gradeButton setTitle:title forState:(UIControlStateNormal)];
     
-    [self.gradeButton animationFromnum:0 toNum:[title floatValue] duration:2.f];
+    [self.gradeButton animationFromnum:0 toNum:[title floatValue] duration:1.3f];
+    
+    
+    CGSize textSize = [title jnife_stringSizeWithFont:[UIFont systemFontOfSize:18]];
+    
+    [UIView animateWithDuration:.8 delay:0 usingSpringWithDamping:.3 initialSpringVelocity:.7 options:
+     UIViewAnimationOptionCurveLinear animations:^{
+         
+         CGRect tempRect;
+         
+         tempRect = CGRectMake(0,0, textSize.width + 50, 30);
+         
+         [self.gradeButton setFrame:tempRect];
+         
+         [self.gradeButton setCenter:CGPointMake(65, 15)];
+         
+         
+     } completion:^(BOOL finished) {
+         
+         
+         
+     }];
+    
     
 }
 
